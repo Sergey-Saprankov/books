ed2d8aa5602d82ec4b93bc2e97d107d67af56729
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-03-25 00:54:03 +0300

    fix bag

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,8 @@
 import './styles/index.scss'
-import { useEffect, useMemo } from 'react'
-
-import { useSelector } from 'react-redux'
 
 import { useTheme } from 'app/providers/ThemeProvider/lib/useTheme'
-import { getBooks, getIsLoading, getParams } from 'entities/books'
-import { fetchBooks } from 'entities/books/model/services/fetchBooks'
 import { MainPage } from 'pages/MainPage'
 import { classNames } from 'shared/lib/classNames/classNames'
-import { API_KEY } from 'shared/lib/const/apiKey'
-import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch'
-import { Loader } from 'shared/ui/Loader/Loader'
 import { Header } from 'widgets/Header/ui/Header'
 
 function App() {
