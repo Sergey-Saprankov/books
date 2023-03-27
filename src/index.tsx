fde5b2f7bbf041690ded17965b66456069b2aee9
Sergey-Saprankov <sergey-saprankov@mail.ru>
2023-03-27 14:18:34 +0300

    fix bags

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 import reportWebVitals from './reportWebVitals'
 
@@ -14,13 +14,13 @@ const root = createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <StoreProvider>
-    <BrowserRouter>
+    <HashRouter>
       <ErrorBoundary>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </ErrorBoundary>
-    </BrowserRouter>
+    </HashRouter>
   </StoreProvider>
 )
 
